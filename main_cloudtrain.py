9e1b5a54c15cfcf0c1fb85cddbaf0bdd876fb861
hongi <caohpone@gmail.com>
2021-07-09 14:43:10 +0200

    fix redis_paramsters

---
+++ main_cloudtrain.py
@@ -0,0 +1,52 @@
+import argparse
+import os
+
+from realips.remote.cloud_trainer import CloudTrainerDDPG, CloudTrainerDDPGParams
+from utils import *
+
+def main_ddpg(p):
+    cloude_ddpg = CloudTrainerDDPG(p)
+    cloude_ddpg.run()
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--gpu', action='store_true', help='Activate usage of GPU')
+    parser.add_argument('--generate_config', action='store_true', help='Enable to write default config only')
+    parser.add_argument('--config', default=None, help='Path to config file')
+    parser.add_argument('--id', default=None, help='If set overrides the logfile name and the save name')
+    parser.add_argument('--force', action='store_true', help='Override log file without asking')
+    parser.add_argument('--weights', default=None, help='Path to pretrained weights')
+    parser.add_argument('--params', nargs='*', default=None)
+
+    args = parser.parse_args()
+
+    if args.generate_config:
+        generate_config(CloudTrainerDDPGParams(), "config/default_cloud_ddpg.json")
+        exit("cloud_ddpg_config file generated")
+
+    if not args.gpu:
+        os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+    if args.config is None:
+        exit("config file needed")
+
+    params = read_config(args.config)
+
+    if args.params is not None:
+        params = override_params(params, args.params)
+
+    if args.id is not None:
+        params.stats_params.model_name = args.id
+        params.stats_params.log_file_name = args.id
+
+    if args.force:
+        params.stats_params.force_override = True
+
+    if args.weights is not None:
+        params.stats_params.weights_path = args.weights
+
+    main_ddpg(params)
+
