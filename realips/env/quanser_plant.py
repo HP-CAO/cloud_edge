770883a6a1b1c07b1de156e231c405803990474e
hongi <caohpone@gmail.com>
2021-07-09 14:59:52 +0200

    fix small issue

--- realips/env/quanser_plant.py
+++ realips/env/quanser_plant.py
@@ -72,7 +72,7 @@ class QuanserPlant:
         x_old_rescaled = self.rescale_x(x_old)
         theta_old_rescaled = self.rescale_theta(theta_old)
 
-        self.card.task_read_encoder(self.encoder_read_task, self.num_samples_to_read, self.analog_buffer)
+        self.card.task_read_encoder(self.encoder_read_task, self.num_samples_to_read, self.encoder_buffer)
         x_new, theta_new = self.encoder_buffer
         x_new_rescaled = self.rescale_x(x_new)
         theta_new_rescaled = self.rescale_theta(theta_new)
