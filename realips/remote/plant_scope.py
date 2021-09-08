3ec5b48eb00321ada298565fa3aa46d442b9489e
hongi-t480 <caohpone@gmail.com>
2021-09-08 08:11:15 +0200

    fix one step delay

--- realips/remote/plant_scope.py
+++ realips/remote/plant_scope.py
@@ -2,6 +2,7 @@ from realips.env.gym_physics import GymPhysics, GymPhysicsParams
 from realips.remote.redis import RedisParams, RedisConnection
 from realips.remote.transition import TrajectorySegment
 import pickle
+import tensorflow as tf
 
 
 class PlantScopeParams:
@@ -19,6 +20,7 @@ class PlantScope:
             channel=self.params.redis_params.ch_plant_trajectory_segment)
         self.edge_trajectory_subscriber = self.redis_connection.subscribe(
             channel=self.params.redis_params.ch_edge_trajectory)
+        self.tf_monitor = tf.summary.create_file_writer("./logs/scope")
 
     def receive_plant_trajectory(self):
         plant_states_pack = self.states_subscriber.parse_response()[2]
@@ -33,8 +35,13 @@ class PlantScope:
     def visualize_states(self):
 
         print("Connected, waiting for messages")
-
+        step = 0
         while True:
             states = self.receive_plant_trajectory()
             print(states)
             self.physics.render(states=states)
+
+            # edge_seg = self.receive_edge_trajectory()
+            # with self.tf_monitor.as_default():
+            #     tf.summary.scalar('action_live', edge_seg.last_action, step)
+            # step += 1
