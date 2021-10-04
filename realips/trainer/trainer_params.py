45ecf254c53700a267afa9c5ed4358c374a51582
hongi-t480 <caohpone@gmail.com>
2021-10-04 11:31:47 +0200

    sort configs

--- realips/trainer/trainer_params.py
+++ realips/trainer/trainer_params.py
@@ -3,8 +3,8 @@ class OffPolicyTrainerParams:
         self.gamma_discount = 0.99
         self.rm_size = 1000000
         self.batch_size = 128
-        self.learning_rate_actor = 0.0001
-        self.learning_rate_critic = 0.0001
+        self.learning_rate_actor = 0.0003
+        self.learning_rate_critic = 0.0003
         self.is_remote_train = False
         self.actor_freeze_step_count = 5000
         self.use_prioritized_replay = False
