91603744d24d0b65c0ef7f0af120fe24a922a402
hongi <caohpone@gmail.com>
2021-11-09 13:54:20 +0100

    modify configs

--- run_sim_30hz_force.sh
+++ run_sim_30hz_force.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-python main_ddpg.py --config ./config/sim_30hz/sim_30hz_force/sim_ddpg_30hz_okay_friction.json --id sim_ddpg_30hz_okay_friction --gpu --force &
-python main_ddpg.py --config ./config/sim_30hz/sim_30hz_force/sim_ddpg_+_30hz_okay_friction.json --id sim_ddpg_+_30hz_okay_friction --gpu --force &
-python main_ddpg.py --config ./config/sim_30hz/sim_30hz_force/sim_ddpg_++_30hz_okay_friction.json --id sim_ddpg_++_30hz_okay_friction --gpu --force &
-python main_td3.py --config ./config/sim_30hz/sim_30hz_force/sim_td3_30hz_okay_friction.json --id sim_td3_30hz_okay_friction --gpu --force
+python main_ddpg.py --config ./config/sim_30hz/sim_30hz_force/sim_ddpg_30hz_okay_friction.json --id sim_ddpg_30hz_okay_friction_force --gpu --force &
+python main_ddpg.py --config ./config/sim_30hz/sim_30hz_force/sim_ddpg_+_30hz_okay_friction.json --id sim_ddpg_+_30hz_okay_friction_force --gpu --force &
+python main_ddpg.py --config ./config/sim_30hz/sim_30hz_force/sim_ddpg_++_30hz_okay_friction.json --id sim_ddpg_++_30hz_okay_friction_force --gpu --force &
+python main_td3.py --config ./config/sim_30hz/sim_30hz_force/sim_td3_30hz_okay_friction.json --id sim_td3_30hz_okay_friction_force --gpu --force
--- run_sim_30hz_voltage.sh
+++ run_sim_30hz_voltage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-python main_ddpg.py --config ./config/sim_30hz/sim_30hz_voltage/sim_ddpg_30hz_okay_friction.json --id sim_ddpg_30hz_okay_friction --gpu --force &
-python main_ddpg.py --config ./config/sim_30hz/sim_30hz_voltage/sim_ddpg_+_30hz_okay_friction.json --id sim_ddpg_+_30hz_okay_friction --gpu --force &
-python main_ddpg.py --config ./config/sim_30hz/sim_30hz_voltage/sim_ddpg_++_30hz_okay_friction.json --id sim_ddpg_++_30hz_okay_friction --gpu --force &
-python main_td3.py --config ./config/sim_30hz/sim_30hz_voltage/sim_td3_30hz_okay_friction.json --id sim_td3_30hz_okay_friction --gpu --force
+python main_ddpg.py --config ./config/sim_30hz/sim_30hz_voltage/sim_ddpg_30hz_okay_friction.json --id sim_ddpg_30hz_okay_friction_voltage --gpu --force &
+python main_ddpg.py --config ./config/sim_30hz/sim_30hz_voltage/sim_ddpg_+_30hz_okay_friction.json --id sim_ddpg_+_30hz_okay_friction_voltage --gpu --force &
+python main_ddpg.py --config ./config/sim_30hz/sim_30hz_voltage/sim_ddpg_++_30hz_okay_friction.json --id sim_ddpg_++_30hz_okay_friction_voltage --gpu --force &
+python main_td3.py --config ./config/sim_30hz/sim_30hz_voltage/sim_td3_30hz_okay_friction.json --id sim_td3_30hz_okay_friction_voltage --gpu --force
