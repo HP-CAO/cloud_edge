804687717cceb3a0dcc6ebbefa3c2d7290b432b4
Mirco Theile <mirco.theile@tum.de>
2021-12-13 15:19:22 +0100

    Added log eval and fixed bug in cloud_trainer

---
+++ evaluate_logs.py
@@ -0,0 +1,56 @@
+import json
+
+import tqdm
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+import os
+
+
+def get_timeseries(event, ts_id):
+    _, steps, vals = zip(*event.Tensors(ts_id))
+    value = [tf.make_ndarray(val).tolist() for val in vals]
+    return steps, value
+
+
+dirs = [x[0] for x in os.walk('logs')]
+
+stats = {}
+
+for d in tqdm.tqdm(dirs):
+    if not d.endswith('training') and not d.endswith('eval'):
+        continue
+    event_acc = EventAccumulator(d, size_guidance={'tensors': 100000})
+    event_acc.Reload()
+
+    log_name = d[d.find('/')+1:d.rfind('/')]
+
+    if log_name not in stats:
+        stats[log_name] = {}
+
+    log_stats = stats[log_name]
+
+    if d.endswith('training'):
+        continue
+    if d.endswith('eval'):
+        try:
+            s, v = get_timeseries(event_acc, 'swing_up_time')
+            if len(s) < 5:
+                continue
+        except KeyError:
+            continue
+
+        if np.all(np.array(v[-5:]) < 250.0):
+            log_stats["convergence_time"] = s[-5]
+        else:
+            log_stats["convergence_time"] = -1
+
+        log_stats["swing_up_time_series"] = {}
+        for step, value in zip(s, v):
+            log_stats["swing_up_time_series"][step] = value
+
+with open('data.json', 'w') as f:
+    json.dump(stats, f)
