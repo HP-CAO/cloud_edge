5897e90d7666416f3f0cd7ba6104f95b838898d8
hongi <caohpone@gmail.com>
2022-01-27 18:30:39 +0100

    new

--- main_cloudtrain.py
+++ main_cloudtrain.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-
+import tensorflow as tf
 from realips.remote.cloud_trainer import CloudSystem, CloudSystemParams
 from utils import *
 
@@ -40,6 +40,12 @@ if __name__ == '__main__':
 
     if not args.gpu:
         os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+    else:
+        physical_devices = tf.config.list_physical_devices('GPU')
+        try:
+            tf.config.experimental.set_memory_growth(physical_devices[0], True)
+        except:
+            exit("GPU allocated failed")
 
     if args.config is None:
         exit("config file needed")
--- real_exp_sh/real_sup_5_fric.sh
+++ real_exp_sh/real_sup_5_fric.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-python main_cloudtrain.py --config ./config/remote/remote_cloud_ddpg+.json --weights ./models/real_sup/sim_ddpg_+_non_friction_sup_1_best_DDPG/ --id real_non_sup_1_5th --gpu
-python main_cloudtrain.py --config ./config/remote/remote_cloud_ddpg+.json --weights ./models/real_sup/sim_ddpg_+_low_friction_sup_1_best_DDPG/ --id real_low_sup_1_5th --gpu
-python main_cloudtrain.py --config ./config/remote/remote_cloud_ddpg+.json --weights ./models/real_sup/sim_ddpg_+_okay_friction_sup_1_best_DDPG/ --id real_okay_sup_1_5th --gpu
-python main_cloudtrain.py --config ./config/remote/remote_cloud_ddpg+.json --weights ./models/real_sup/sim_ddpg_+_high_friction_sup_1_best_DDPG/ --id real_high_sup_1_5th --gpu
-python main_cloudtrain.py --config ./config/remote/remote_cloud_ddpg+.json --weights ./models/real_sup/sim_ddpg_+_over_friction_sup_1_best_DDPG/ --id real_over_sup_1_5th --gpu
+python main_cloudtrain.py --config ./config/remote/remote_cloud_ddpg+.json --weights ./models/sup_sim/sim_ddpg_+_low_friction_sup_1_best_DDPG/ --id real_low_sup_1_5th --gpu
+python main_cloudtrain.py --config ./config/remote/remote_cloud_ddpg+.json --weights ./models/sup_sim/sim_ddpg_+_okay_friction_sup_1_best_DDPG/ --id real_okay_sup_1_5th --gpu
+python main_cloudtrain.py --config ./config/remote/remote_cloud_ddpg+.json --weights ./models/sup_sim/sim_ddpg_+_high_friction_sup_1_best_DDPG/ --id real_high_sup_1_5th --gpu
+python main_cloudtrain.py --config ./config/remote/remote_cloud_ddpg+.json --weights ./models/sup_sim/sim_ddpg_+_over_friction_sup_1_best_DDPG/ --id real_over_sup_1_5th --gpu
+python main_cloudtrain.py --config ./config/remote/remote_cloud_ddpg+.json --weights ./models/sup_sim/sim_ddpg_+_non_friction_sup_1_best_DDPG/ --id real_non_sup_1_5th --gpu
