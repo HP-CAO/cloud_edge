f21dfd87652065dbda13393719493cf789985d1a
hongi <caohpone@gmail.com>
2022-01-27 18:30:16 +0100

    new

---
+++ real_exp_sh/real_low_friction_eval.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+python main_cloudtrain.py --config ./config/remote/remote_cloud_ddpg+.json --weights ./models/models_11_11/sim_ddpg+_30hz_okay_friction_voltage_4cm_best_DDPG/ --id real_bandwidth_0.1_no_cer_1sr --gpu --params trainer_params/combined_experience_replay false cloud_params/artificial_bandwidth 0.1
+python main_cloudtrain.py --config ./config/remote/remote_cloud_ddpg+.json --weights ./models/models_11_11/sim_ddpg+_30hz_okay_friction_voltage_4cm_best_DDPG/ --id real_bandwidth_0.1_no_cer_2nd --gpu --params trainer_params/combined_experience_replay false cloud_params/artificial_bandwidth 0.1
+python main_cloudtrain.py --config ./config/remote/remote_cloud_ddpg+.json --weights ./models/models_11_11/sim_ddpg+_30hz_okay_friction_voltage_4cm_best_DDPG/ --id real_bandwidth_0.1_no_cer_3rd --gpu --params trainer_params/combined_experience_replay false cloud_params/artificial_bandwidth 0.1
